1eb8c2eda3d3152909653f99b6fcd0ac75eae3d7
George Roman <30772943+georgeroman@users.noreply.github.com>
2025-07-29 11:56:00 +0200

    feat: add support for api keys

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,14 @@
 export const config = {
   httpPort: Number(process.env.HTTP_PORT!),
-
   postgresUrl: process.env.POSTGRES_URL!,
-
   ecdsaPrivateKey: process.env.ECDSA_PRIVATE_KEY!,
   ed25519PrivateKey: process.env.ED25519_PRIVATE_KEY!,
+  apiKeys: process.env.API_KEYS
+    ? Object.fromEntries(
+        process.env.API_KEYS.split(";").map((apiKey) => {
+          const [key, integrator] = apiKey.split(":");
+          return [key, integrator];
+        })
+      )
+    : undefined,
 };
