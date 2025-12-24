7fa176983be1fae8aa51996f7391b521c9810cd9
FIlipe <filipe@unevenlabs.com>
2025-12-24 11:47:34 +0000

    feat: enable eslint (#108)
    Signed-off-by: Filipe Mota <filipe@unevenlabs.com>

--- src/scripts/run-migrations.ts
+++ src/scripts/run-migrations.ts
@@ -1,13 +1,13 @@
 // wrapper for node-pg-migrate to inject the database password
-import { spawnSync } from 'node:child_process';
-import { getDatabaseUrlWithPassword } from '../common/db'
+import { spawnSync } from "node:child_process";
+import { getDatabaseUrlWithPassword } from "../common/db"
 
 (async () => {
   process.env.POSTGRES_URL = await getDatabaseUrlWithPassword(String(process.env.POSTGRES_URL));
 
   spawnSync(
-    'node-pg-migrate',
+    "node-pg-migrate",
     process.argv.slice(2),
-    { stdio: 'inherit' }
+    { stdio: "inherit" }
   );
 })();
