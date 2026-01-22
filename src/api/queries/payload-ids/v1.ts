91edeaae0bf84176298bd926029a8ef3d98cc177
Clément Renaud <clement.renaud@gmail.com>
2026-01-22 15:15:44 +0100

    support a default `allocatorChainId` for unsupported VMs (#120)

--- src/api/queries/payload-ids/v1.ts
+++ src/api/queries/payload-ids/v1.ts
@@ -70,11 +70,16 @@ export default {
   ) => {
     const chain = await getChain(req.body.chainId);
 
+    // we add a default value so the endpoint do not break 
+    // and return a value for vm that are not supported
+    // by current allocator (like Tron or Bitcoin)
+    const allocatorChainId = chain.metadata.allocatorChainId || "0n"
+
     const requestHandler = new RequestHandlerService();
     const payloadParams = requestHandler.parseAllocatorPayloadParams(
       chain.vmType,
       chain.depository!,
-      chain.metadata.allocatorChainId!,
+      allocatorChainId,
       req.body.currency,
       req.body.amount,
       req.body.recipient,
